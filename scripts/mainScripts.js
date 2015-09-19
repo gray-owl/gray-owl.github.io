ea212025ef335aadd8516aaeed226faa488ff4ba
gray-owl <youllprobablysendmespam@gmail.com>
2015-09-19 09:34:55 -0500

    Create mainScripts.js

---
+++ scripts/mainScripts.js
@@ -0,0 +1,35 @@
+
+$('.bxslider').bxSlider({
+  adaptiveHeight: true,
+  mode: 'fade'
+});
+
+function myFunction() {
+    document.getElementById("demo").style.color = "red";
+    //document.getElementById("txtOne").value = "Chicken Pot Pie"
+    alert(document.getElementById("txtOne").value);
+}
+
+
+
+/*$(document).click(function () {
+      $("#toggle").effect("highlight", {}, 3000);
+});
+*/
+
+
+
+
+$(document).ready(function(){
+	
+	$( document ).click(function() {
+		$( "#toggle" ).toggle( "highlight" );
+	});
+	
+	$( "#actionButton" ).click(function() {
+		//alert("Here is some text!")
+		$( "#book" ).slideToggle( "slow" );
+		$("#actionButton").text("jQUERY VOO-DOO FOOL!!!!")
+	});
+
+});
