cf1bc0816cb280a306f020880e7598b37bc597b3
gray-owl <youllprobablysendmespam@gmail.com>
2015-09-21 20:00:13 -0500

    Update mainScripts.js

--- scripts/mainScripts.js
+++ scripts/mainScripts.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
   		adaptiveHeight: true,
   		mode: 'fade'
 	});*/
+	alert("Here is some text!")
 	$('.bxslider').bxSlider({
-  		mode: 'fade',
+		mode: 'fade',
   		auto: true,
   		autoControls: true,
   		pause: 2000
 	});
+	alert("Here is some more text!")
 	$( document ).click(function() {
 		$( "#toggle" ).toggle( "highlight" );
 	});
